c0d115e2d18a5992aae0c6e887418721e39be94c
Mako Adam Peter <makoadampeter@gmail.com>
2025-04-10 22:38:50 +0200

    build: generate download-form component

---
+++ src/app/download-form/download-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-download-form',
+  imports: [],
+  templateUrl: './download-form.component.html',
+  styleUrl: './download-form.component.scss'
+})
+export class DownloadFormComponent {
+
+}
